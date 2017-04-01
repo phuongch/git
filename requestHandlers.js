52d3d17802876e22b675bb6676d0a46a8ed2c845
cao hoai phuong <caohoaiphuong90@gmail.com>
2017-04-01 15:40:47 +0700

    Change Handler

--- requestHandlers.js
+++ requestHandlers.js
@@ -23,7 +23,7 @@ function start(response, postData){
 function upload(response, postData){
   console.log("Request handler 'upload' was called.");	 
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent: "+ 
+  response.write("You've sent data: "+ 
   querystring.parse(postData).text);
   response.end();
 }
